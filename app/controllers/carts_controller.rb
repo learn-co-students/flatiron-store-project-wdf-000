263f56edf32e0d641cd8ce9fd36965aad04343df
Raycent <raycent.tan@flatironschool.com>
2016-09-05 11:40:34 -0400

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,12 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!
 
+  def show
+    @user = current_user
+  end
+
+  def checkout
+    current_cart = Cart.find(params[:id])
+    current_cart.checkout
+  end
 end
