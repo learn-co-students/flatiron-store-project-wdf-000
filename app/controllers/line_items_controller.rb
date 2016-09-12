14553bbea45a4bb54550693e8acb16224eb657fd
Margaret <eutopia@gmail.com>
2016-09-12 16:10:18 -0400

    Done.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,18 @@
+require 'pry'
 class LineItemsController < ApplicationController
-
+	def create
+		if current_user
+			if current_user.current_cart
+				@cart = current_user.current_cart
+				@cart.save
+			else 
+				current_user.current_cart=(Cart.create(user_id: current_user.id))
+				current_user.save
+				@cart = current_user.current_cart
+			end
+			@cart.add_item(params[:item_id])
+			@cart.save
+			redirect_to @cart
+		end
+	end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,28 @@
 class Cart < ActiveRecord::Base
+	has_many :line_items
+	has_many :items, through: :line_items
+	belongs_to :user
 
+	def total
+		line_items.inject(0) {|t, i| t += i.item.price * i.quantity}
+	end
+
+	def add_item(item)
+    line_item = self.line_items.where(item: item).first
+    if line_item
+      line_item.quantity += 1
+      line_item.save
+    else line_item = self.line_items.build(item_id: item)
+    end
+    line_item
+  end
+
+	def checkout
+		self.line_items.each do |l|
+			l.item.inventory -= l.quantity
+			l.item.save
+		end
+		self.user.current_cart_id = nil
+		self.user.save
+	end
 end
