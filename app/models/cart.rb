c71c7d1a7edcbb14c92de17185f12ea553a32213
ozPop <ozziepopovas@gmail.com>
2016-10-05 17:04:44 -0400

    add total & add_item methods to cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,14 @@ class Cart < ActiveRecord::Base
   belongs_to :user
   has_many :line_items
   has_many :items, through: :line_items
+
+  def total
+    line_items.collect(&:item).collect(&:price).inject(:+).to_f
+  end
+
+  def add_item(item_id)
+    line_item = LineItem.find_or_initialize_by(item_id: item_id)
+    line_item.cart_id = id unless line_item.cart_id
+    line_item
+  end
 end
