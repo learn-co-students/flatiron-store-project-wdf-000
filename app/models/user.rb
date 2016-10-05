450a66a8373dadc2422dd20e7c9320cdf6a16f6c
ozPop <ozziepopovas@gmail.com>
2016-10-05 15:38:40 -0400

    add current_cart migration, update user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :carts
+  belongs_to :current_cart, class_name: 'Cart'
 end
