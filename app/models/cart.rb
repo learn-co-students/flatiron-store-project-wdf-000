f39948da370c5fb336688709e1ebfe1eed31f229
fidelscodes <fidelscodes@gmail.com>
2016-10-05 12:58:29 -0400

    pass cart spec

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,13 @@ class Cart < ActiveRecord::Base
   has_many :line_items
   has_many :items, through: :line_items
 
+  def total
+    items.map(&:price).reduce(:+)
+  end
+
+  def add_item(item_id)
+    # LineItem.where(cart: self, item_id: item_id).first_or_initialize
+    LineItem.find_or_initialize_by(cart: self, item_id: item_id)
+  end
+
 end
