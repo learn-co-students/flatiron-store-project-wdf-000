27cc9b2be201092573c2acf634c4ec8fa992a709
Flatiron School <ellis.paige.v@gmail.com>
2016-10-06 09:07:59 -0400

    added status change to #checkout in carts_controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,6 +6,7 @@ class CartsController < ApplicationController
   def checkout
     cart = Cart.find(params[:id])
     cart.update_inventory
+    cart.status = "submitted"
     current_user.current_cart = nil
     current_user.save
     redirect_to cart_path(cart)
