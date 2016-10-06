038bd42a4d6c19cb5f73fd3547e724c35ef98baa
ozPop <ozziepopovas@gmail.com>
2016-10-06 15:30:59 -0400

    refactor #line_items, header partial

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,19 +1,14 @@
 class LineItemsController < ApplicationController
   def create
-    @cart = current_cart
     # assign cart unless already assigned
-    current_user.current_cart = @cart unless current_user.current_cart
-    # why are duplicates not getting created?
-    @cart.line_items << @cart.add_item(params[:item_id])
-
-    # does this make sense?
-    # line_item = @cart.add_item(params[:item_id])
-    # if line_item.new_record?
-    #   @cart.line_items << @cart.add_item(params[:item_id])
-    # else
-    #   @cart.line_items.find(id: line_item.id).update(quatity: line_item.quatity)
-    # end
-
-    redirect_to cart_path(@cart.id)
+    current_user.create_cart unless current_user.current_cart
+    line_item = current_user.current_cart.add_item(params[:item_id])
+    if line_item.save
+      redirect_to cart_path(current_user.current_cart.id), notice: 'Item added to cart!'
+    else
+      redirect_to store_path, alert: 'Item not added'
+    end
+    # raise 'stop'.inspect
+    # redirect_to cart_path(current_user.current_cart.id)
   end
 end
