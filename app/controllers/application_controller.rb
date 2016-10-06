fc54339c124d7537c9633d77a3be2b22063bc3c8
ozPop <ozziepopovas@gmail.com>
2016-10-06 15:29:21 -0400

    remove current cart method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_cart
-
-  def current_cart
-    Cart.find_or_create_by(user: current_user)
-  end
 end
