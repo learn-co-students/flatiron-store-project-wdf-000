4566c9309ee14fd6e98df9052e990712149355bd
Wendy Xu <guangw.xu@gmail.com>
2016-10-12 18:56:58 -0400

    finshed a few tests

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,3 +1,9 @@
 class StoreController < ApplicationController
-  
+
+  def index
+    # binding.pry
+    @categories = Category.all
+    @items = Item.all.find_all {|item| item.inventory > 0}
+
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def number_to_currency(price)
+    "$#{price.to_f/100}"
+  end
+
 end
