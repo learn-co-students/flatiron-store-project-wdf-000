7fa35aaf6f510700de7ec7ee4670460569586e6e
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-10-25 09:20:07 -0400

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,15 @@
 class CartsController < ApplicationController
+  before_action :authenticate_user!
+  def show
+
+  end
+
+  def checkout
+    cart = Cart.find_by(id: params[:id])
+    cart.checkout
+    session[:checkout] = true
+    redirect_to cart_path(cart)
+  end
+
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def number_to_currency(price)
+    "$#{price.to_f/100}"
+  end
+
+  def current_cart
+    if session[:checkout]
+      Cart.find_by(id: params[:id])
+    else
+      current_user.try(:current_cart)
+    end
+  end
 end
