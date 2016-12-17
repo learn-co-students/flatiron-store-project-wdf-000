3945e64885538d4acf2106381205a2a528a5c17f
Keo <keonilrath@gmail.com>
2016-12-17 02:30:37 -0500

    Done.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
   def create
     if user_signed_in?
-      current_user.current_cart = current_user.carts.new unless current_cart.present?
+      current_user.current_cart = current_user.carts.new unless current_user.current_cart
       current_user.save
       current_cart.add_item(params[:item_id]).save
       redirect_to cart_path(current_cart)
